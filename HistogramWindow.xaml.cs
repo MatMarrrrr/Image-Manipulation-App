5c7e87eab7a25b43b29beef73036e57b2507be49
MatMarrr <mateuszrmarek@gmail.com>
2024-03-10 00:07:34 +0100

    Add pixels histogram display

--- HistogramWindow.xaml.cs
+++ HistogramWindow.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using Emgu.CV;
+using Emgu.CV.Reg;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -46,8 +50,30 @@ namespace APO_Mateusz_Marek_20456
             {
                 histogramTableData.Add(new HistogramDataRow { Intensity = i, Count = histogramData[i] });
             }
+            HistogramTable.ItemsSource = histogramTableData;
 
-            pixelDataTable.ItemsSource = histogramTableData;
+
+            Bitmap imageBitmap = imageMat.ToBitmap();
+            var pixelsData = new List<List<int>>();
+
+            for (int y = 0; y < imageBitmap.Height; ++y)
+            {
+                var row = new List<int>();
+                for (int x = 0; x < imageBitmap.Width; ++x)
+                {
+                    System.Drawing.Color c = imageBitmap.GetPixel(x, y);
+                    int intensity = (int)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
+                    row.Add(intensity);
+                }
+                pixelsData.Add(row);
+            }
+
+            for (int i = 0; i < imageBitmap.Width; i++)
+            {
+                HistogramPixels.Columns.Add(new DataGridTextColumn { Header = $"Col {i + 1}", Binding = new Binding($"[{i}]") });
+            }
+
+            HistogramPixels.ItemsSource = pixelsData;
 
             Show();
         }
