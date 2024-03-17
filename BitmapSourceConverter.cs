1901d39e9f52c429e6b084ce7d148448e80561fa
MatMarrr <mateuszrmarek@gmail.com>
2024-03-17 21:51:48 +0100

    Delete unused commented method

--- BitmapSourceConverter.cs
+++ BitmapSourceConverter.cs
@@ -15,25 +15,6 @@ namespace APO_Mateusz_Marek_20456
 {
     internal static class BitmapSourceConverter
     {
-        /*
-        public static BitmapSource ToBitmapSource(Mat mat)
-        {
-            Bitmap bitmap = mat.ToBitmap();
-            MemoryStream stream = new MemoryStream();
-
-            bitmap.Save(stream, ImageFormat.Bmp);
-            stream.Position = 0;
-
-            BitmapImage bitmapImage = new BitmapImage();
-            bitmapImage.BeginInit();
-            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-            bitmapImage.StreamSource = stream;
-            bitmapImage.EndInit();
-            bitmapImage.Freeze();
-
-            return bitmapImage;
-        }
-        */
         public static BitmapSource ToBitmapSource(Mat mat)
         {
             return Imaging.CreateBitmapSourceFromHBitmap(
